b5d96e6038a349fbcc72f805c5e0bff8a55ef256
Michael Bestas <mkbestas@gmail.com>
2023-06-14 11:53:23 +0300

    SystemUI: Restore wifi and cellular QS tiles
    Co-authored-by: Tommaso Fonda <fondatommaso2@gmail.com>
    Change-Id: Ibbbabf30be08c6852f6aa968d28de94ac29b0e71

--- packages/SystemUI/src/com/android/systemui/statusbar/policy/PolicyModule.kt
+++ packages/SystemUI/src/com/android/systemui/statusbar/policy/PolicyModule.kt
@@ -32,6 +32,7 @@ import com.android.systemui.qs.tileimpl.QSTileImpl
 import com.android.systemui.qs.tiles.AlarmTile
 import com.android.systemui.qs.tiles.AODTile
 import com.android.systemui.qs.tiles.CaffeineTile
+import com.android.systemui.qs.tiles.CellularTile
 import com.android.systemui.qs.tiles.CameraToggleTile
 import com.android.systemui.qs.tiles.DcDimTile
 import com.android.systemui.qs.tiles.FlashlightTile
@@ -46,6 +47,7 @@ import com.android.systemui.qs.tiles.PeakRefreshTile
 import com.android.systemui.qs.tiles.PowerShareTile
 import com.android.systemui.qs.tiles.SoundTile
 import com.android.systemui.qs.tiles.UiModeNightTile
+import com.android.systemui.qs.tiles.WifiTile
 import com.android.systemui.qs.tiles.WorkModeTile
 import com.android.systemui.qs.tiles.base.domain.interactor.QSTileAvailabilityInteractor
 import com.android.systemui.qs.tiles.base.shared.model.QSTileConfig
@@ -657,6 +659,12 @@ interface PolicyModule {
     @StringKey(CaffeineTile.TILE_SPEC)
     fun bindCaffeineTile(caffeineTile: CaffeineTile): QSTileImpl<*>
 
+    /** Inject CellularTile into tileMap in QSModule */
+    @Binds
+    @IntoMap
+    @StringKey(CellularTile.TILE_SPEC)
+    fun bindCellularTile(cellularTile: CellularTile): QSTileImpl<*>
+
     @Binds
     @IntoMap
     @StringKey(GamingModeTile.TILE_SPEC)
@@ -687,4 +695,10 @@ interface PolicyModule {
     @IntoMap
     @StringKey(SoundTile.TILE_SPEC)
     fun bindSoundTile(soundTile: SoundTile): QSTileImpl<*>
+
+    /** Inject WifiTile into tileMap in QSModule */
+    @Binds
+    @IntoMap
+    @StringKey(WifiTile.TILE_SPEC)
+    fun bindWifiTile(wifiTile: WifiTile): QSTileImpl<*>
 }
