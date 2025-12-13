225d65a337474f3b26143633e793705a105e1130
John Macka <macka2004@gmail.com>
2025-12-13 12:24:33 +1000

    Revert "KeyboxImitationHooks: Abandon generated certs"
    This reverts commit aab81d05b449b1c98343cbdc6b3c5066c8c48e7a.

--- keystore/java/android/security/KeyStoreSecurityLevel.java
+++ keystore/java/android/security/KeyStoreSecurityLevel.java
@@ -34,6 +34,9 @@ import android.system.keystore2.KeyMetadata;
 import android.system.keystore2.ResponseCode;
 import android.util.Log;
 
+import com.android.internal.util.yaap.KeyboxImitationHooks;
+import com.android.internal.util.yaap.KeyboxUtils;
+
 import java.util.Calendar;
 import java.util.Collection;
 
@@ -152,6 +155,15 @@ public class KeyStoreSecurityLevel {
             if (kp.tag == Tag.ATTESTATION_CHALLENGE) {
                 attestationChallenge = kp.value.getBlob();
                 break;
+            }
+        }
+
+        KeyboxUtils.remove(Binder.getCallingUid(), descriptor.alias);
+        if (attestationKey == null) {
+            KeyMetadata metadata = KeyboxImitationHooks.generateKey(mSecurityLevel,
+                    descriptor, args);
+            if (metadata != null) {
+                return metadata;
             }
         }
 
