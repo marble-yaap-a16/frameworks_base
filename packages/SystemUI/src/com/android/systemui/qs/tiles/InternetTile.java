e36ebb379968f752a3ce0c3e4d6f98ba3ae6ddba
John Macka <macka2004@gmail.com>
2025-12-14 17:40:44 +1000

    Revert "SystemUI: Allow Wi-Fi/cell tiles to co-exist with provider model"
    This reverts commit ac47166cc94fa75e32ed9d7184fb4a1d5d9a5a33.

--- packages/SystemUI/src/com/android/systemui/qs/tiles/InternetTile.java
+++ packages/SystemUI/src/com/android/systemui/qs/tiles/InternetTile.java
@@ -331,9 +331,6 @@ public class InternetTile extends SecureQSTile<QSTile.BooleanState> {
             if (DEBUG) {
                 Log.d(TAG, "setWifiIndicators: " + indicators);
             }
-            if (!indicators.isDefault) {
-                return;
-            }
             synchronized (mWifiInfo) {
                 mWifiInfo.mEnabled = indicators.enabled;
                 mWifiInfo.mSsid = indicators.description;
@@ -359,7 +356,7 @@ public class InternetTile extends SecureQSTile<QSTile.BooleanState> {
             if (DEBUG) {
                 Log.d(TAG, "setMobileDataIndicators: " + indicators);
             }
-            if (indicators.qsIcon == null || !indicators.isDefault) {
+            if (indicators.qsIcon == null) {
                 // Not data sim, don't display.
                 return;
             }
