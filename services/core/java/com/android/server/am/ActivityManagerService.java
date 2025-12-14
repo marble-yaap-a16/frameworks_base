761ece07c297012d3ba51136ff5a7d05f439f524
John Macka <macka2004@gmail.com>
2025-12-14 20:08:13 +1000

    Revert "services: implement app launch optimization tweak"
    This reverts commit 29a675ed079c957f0e8795fa4bb1deb2e95e5e65.

--- services/core/java/com/android/server/am/ActivityManagerService.java
+++ services/core/java/com/android/server/am/ActivityManagerService.java
@@ -347,7 +347,6 @@ import android.os.Looper;
 import android.os.Message;
 import android.os.Parcel;
 import android.os.ParcelFileDescriptor;
-import android.os.PerformanceHintManager;
 import android.os.PowerExemptionManager;
 import android.os.PowerExemptionManager.ReasonCode;
 import android.os.PowerExemptionManager.TempAllowListType;
@@ -988,10 +987,6 @@ public class ActivityManagerService extends IActivityManager.Stub
         final int pid = app.getPid();
         synchronized (mPidsSelfLocked) {
             mPidsSelfLocked.doAddInternal(pid, app);
-            ProcessFreezerManager freezer = ProcessFreezerManager.getInstance();
-            if (freezer != null && freezer.useFreezerManager()) {
-                freezer.addPidLocked(app);
-            }
         }
         synchronized (sActiveProcessInfoSelfLocked) {
             if (app.processInfo != null) {
@@ -1013,11 +1008,6 @@ public class ActivityManagerService extends IActivityManager.Stub
         final boolean removed;
         synchronized (mPidsSelfLocked) {
             removed = mPidsSelfLocked.doRemoveInternal(pid, app);
-            ProcessFreezerManager freezer = ProcessFreezerManager.getInstance();
-            if (freezer != null && freezer.useFreezerManager()) {
-                freezer.removePidLocked(pid, app);
-                freezer.startUnfreeze(app.processName, ProcessFreezerManager.REMOVE_PROCESS_UNFREEZE);
-            }
         }
         if (removed) {
             synchronized (sActiveProcessInfoSelfLocked) {
@@ -5391,7 +5381,6 @@ public class ActivityManagerService extends IActivityManager.Stub
         showMteOverrideNotificationIfActive();
 
         t.traceEnd();
-        ProcessFreezerManager.getInstance().init(mFreezer);
     }
 
     private void showConsoleNotificationIfActive() {
